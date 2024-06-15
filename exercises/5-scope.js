e605cf54f67cd31781501d4735df8290ec23675b
zelcakok <zelca.kok@redso.com.hk>
2024-06-15 18:48:10 +0800

    update: exercise 5

--- exercises/5-scope.js
+++ exercises/5-scope.js
@@ -21,28 +21,3 @@
 // 9. Explain the output of each console.log statement.
 
 // 10. Bonus: Modify the code to explore block scope using 'let' and 'const' variables.
-
-// Solution:
-// 1. let globalVariable = 10;
-
-// 2. function myFunction() {
-//      let localVariable = 5;
-//      let globalVariable = 20;
-
-//      console.log(localVariable); // Output: 5
-//      console.log(globalVariable); // Output: 20
-//    }
-
-// 3. myFunction();
-
-// 4. console.log(localVariable); // Output: ReferenceError: localVariable is not defined
-// 5. console.log(globalVariable); // Output: 10
-
-// 9. Output Explanation:
-// - Inside the 'myFunction' function, the value of 'localVariable' is 5 and the value of 'globalVariable' is 20.
-// - Outside the 'myFunction' function, the value of 'globalVariable' is still 10, as it refers to the global variable defined outside the function.
-// - The 'localVariable' is not accessible outside the 'myFunction' function, hence the ReferenceError.
-
-// 10. Bonus:
-// - Use 'let' or 'const' instead of 'var' to declare variables with block scope.
-// - Modify the code accordingly to explore block scope.
