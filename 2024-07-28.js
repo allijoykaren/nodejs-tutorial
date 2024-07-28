ee0071a3004164f3ab1b29390a47ee5a67f9063a
allijoykaren <nonorenn24a@gmail.com>
2024-07-28 11:46:57 +0000

    update: added 2024-07-28.js

---
+++ 2024-07-28.js
@@ -0,0 +1,43 @@
+const itemPrices = [10, 3, 2, 5, 1, 3, 5];
+let totalPrice = 0;
+
+for (const d of itemPrices) {
+    totalPrice = totalPrice + d}
+console.log(totalPrice);
+
+
+// let five = 2 + 3;
+// console.log(five);
+
+// five = five + 1;
+// console.log(five);
+
+
+
+// const data = [1, 2, 3, 4, 5];
+
+// for (const d of data) {
+//     if (d > 4) {
+//         console.log(d);
+//     }
+// }
+// const data1 = ['a', 'b', 'c', 'd', 'e'];
+
+// for (const d of data1) {
+//     if (d == 'd') {
+//         console.log(d);
+//     }
+// }
+
+
+// const data = [1, 2, 3, 4, 5];
+
+// for (const d of data) {
+//     console.log(d);
+// }
+
+// const data1 = ['a', 'b', 'c', 'd', 'e'];
+// for (const w of data1) {
+//     console.log(w);
+// }
+
